57d27885e1afcb9291c6137c07a1a39afb8e95a5
Jonson Bhowmik <jonson_bhowmik@apple.com>
2021-09-07 05:46:27 +0600

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    ecmaFeatures: {
+      legacyDecorators: true
+    }
+  },
+  extends: 'standard',
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  rules: {
+    'arrow-parens': 0,
+    'generator-star-spacing': 0,
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'no-console': 2
+  },
+  globals: {
+    'use': true
+  }
+}
